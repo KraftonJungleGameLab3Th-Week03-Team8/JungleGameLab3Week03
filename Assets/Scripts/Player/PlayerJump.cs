05ab91962999112432c68be44e02ad08d502cf44
sayishuu <smile52673@gmail.com>
2025-03-19 23:22:46 +0900

    [FEAT] playerJump 구현
    prototype ver
    충전없이 고정된 jumpForce값 상태

--- Assets/Scripts/Player/PlayerJump.cs
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //일단 rigidbody 다 가져올게요. 나중에 리팩토링 ㄱㄱ
+    private Rigidbody2D _rb;
+    private float _jumpForce;
+
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _jumpForce = 15;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        InputManager.Instance.jumpChargeAction += ConstraintPosition;
+        InputManager.Instance.jumpAction += ReleaseConstraint;
+        InputManager.Instance.jumpAction += Jump;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.jumpChargeAction -= ConstraintPosition;
+        InputManager.Instance.jumpAction -= ReleaseConstraint;
+        InputManager.Instance.jumpAction -= Jump;
+    }
+
+    private void ConstraintPosition()
+    {
+        _rb.constraints = RigidbodyConstraints2D.FreezePosition;
+    }
+
+    private void ReleaseConstraint()
+    {
+        _rb.constraints = RigidbodyConstraints2D.None;
+    }
+
+    private void Jump()
+    {
+        _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
     }
 }
