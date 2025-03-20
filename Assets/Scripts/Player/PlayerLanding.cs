9e56101ee824e080cf674ca1f764baf3cb4c6cb7
Dongyeong Kim <dyeongkim98@gmail.com>
2025-03-20 15:04:57 +0900

    [FEAT] PlayerLanding 구현
    Down 이벤트 Canceled 시 랜딩 실행
    _isGround: Space 키 맵핑이 점프와 함께 사용 중 이여서 구문코드 추가

--- Assets/Scripts/Player/PlayerLanding.cs
+++ Assets/Scripts/Player/PlayerLanding.cs
@@ -1,16 +1,31 @@
+using System;
 using UnityEngine;
 
 public class PlayerLanding : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D _rb;
+    private InputManager _inputManager;
+    [SerializeField] private float _downForce;
+
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _downForce = 300f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
+        _inputManager = InputManager.Instance;
+        _inputManager.downAction += OnDownCanceled;
+    }
+
+    private void OnDisable()
+    {
+        _inputManager.jumpChargeAction -= OnDownCanceled;
         
+    }
+    void OnDownCanceled()
+    {
+        _rb.AddForce(Vector2.down * _downForce, ForceMode2D.Impulse);
     }
 }
