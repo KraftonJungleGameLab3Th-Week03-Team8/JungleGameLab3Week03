6bea2fb2a37bf516dbba264b28a4924ce25bfa2d
SMG9 <asnmfg@naver.com>
2025-03-21 23:32:01 +0900

    [ADD] UIKeyGuideDisplay
    초기 작업중

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -2,4 +2,29 @@ using UnityEngine;
 
 public class UIManager
 {
+<<<<<<< Updated upstream
 }
+=======
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 타이틀 화면 캔버스 제어
+        // 타이틀명, 시작버튼?
+    
+    // 플레이 타임 제어
+        // 타이머 시작, 정지/일시정지, 초기화
+    // 슈퍼 히어로 랜딩
+    // 각종 연출
+        // 페이드 in/out
+
+}
+>>>>>>> Stashed changes
