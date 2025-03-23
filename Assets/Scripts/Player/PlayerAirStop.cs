20f1a3703f0f01738834a27b6f44d9cd133f981c
sayishuu <smile52673@gmail.com>
2025-03-23 17:21:13 +0900

    [FEAT] 랜딩 차지할수록 추락속도 증가 구현
    인스펙터 PlayerJump MaxFallSpeed = 120으로 수정 부탁드립니다.

--- Assets/Scripts/Player/PlayerAirStop.cs
+++ Assets/Scripts/Player/PlayerAirStop.cs
@@ -6,8 +6,11 @@ public class PlayerAirStop : MonoBehaviour
     private Rigidbody2D _rb;
     private float _defaultRotateSpeed = 10f;
     [SerializeField] private float _rotateSpeed;
+    public float RotateSpeed { get { return _rotateSpeed; } }
     [SerializeField] private float _rotateSpeedCharge;
     [SerializeField] private float _rotateSpeedMax;
+    [SerializeField] private float _startRotation;
+    public float StartRotation { get { return _startRotation; } }
     [SerializeField] private float _startHeight;
     public float StartHeight { get { return _startHeight; } }
 
@@ -19,7 +22,7 @@ public class PlayerAirStop : MonoBehaviour
 
         _rotateSpeed = 10f;
         _rotateSpeedCharge = 0.5f;
-        _rotateSpeedMax = 25f;
+        _rotateSpeedMax = 30f;
     }
 
     private void FixedUpdate()
@@ -38,14 +41,17 @@ public class PlayerAirStop : MonoBehaviour
     {
         _rb.constraints = RigidbodyConstraints2D.FreezePosition;
         _rotateSpeed += _rotateSpeedCharge;
-        _rb.AddTorque(_rotateSpeed);
+        if (_rotateSpeed <= 20f)
+        {
+            _rb.AddTorque(_rotateSpeed);
+        }
     }
 
     private void OnDownStarted()
     {
         Manager.Game.PlayerController.SetGravityScale(Manager.Game.PlayerController.GravityScale);
         _startHeight = transform.position.y;
-
+        _startRotation = _rotateSpeed;
         _rotateSpeed = _defaultRotateSpeed;
     }
 }
--- Assets/Scripts/Player/PlayerLanding.cs
+++ Assets/Scripts/Player/PlayerLanding.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerLanding : MonoBehaviour
 {
-    private float _landForce = 200f;
+    private float _landForce = 10f;
+    private float _chargedLandForce;
 
     private void Start()
     {
@@ -12,8 +13,9 @@ public class PlayerLanding : MonoBehaviour
     private void Land(Rigidbody2D rb)
     {
         rb.angularVelocity = 0;
+        _chargedLandForce = Manager.Game.PlayerController.PlayerAirStop.RotateSpeed * 2;
         rb.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         rb.constraints = RigidbodyConstraints2D.FreezePositionX;
-        rb.AddForce(Vector2.down * _landForce, ForceMode2D.Impulse);
+        rb.AddForce(Vector2.down * (_landForce + _chargedLandForce), ForceMode2D.Impulse);
     }
 }
