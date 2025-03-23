8105576a2367d95429a6edbe6fc81c5b9afe4db0
bokob <bmc0520@naver.com>
2025-03-23 18:59:02 +0900

    [MERGE] 머지 충돌 해결 2차

--- Assets/Scripts/Player/PlayerLanding.cs
+++ Assets/Scripts/Player/PlayerLanding.cs
@@ -4,7 +4,7 @@ public class PlayerLanding : MonoBehaviour
 {
     private float _landingForce = 10f;  // 랜딩 힘
     private float _chargedLandingForce; // 충전된 랜딩 힘
-    public float ChargedLandForce => _chargedLandForce;
+    public float ChargedLandForce => _chargedLandingForce;
 
     private void Start()
     {
@@ -16,7 +16,7 @@ public class PlayerLanding : MonoBehaviour
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         rb.constraints = RigidbodyConstraints2D.FreezePositionX;
         rb.angularVelocity = 0;
-        _chargedLandForce = Manager.Game.PlayerController.PlayerAirStop.CurrentRotation / 200;
+        _chargedLandingForce = Manager.Game.PlayerController.PlayerAirStop.CurrentRotation / 200;
         rb.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         rb.AddForce(Vector2.down * (_landingForce + _chargedLandingForce), ForceMode2D.Impulse);
     }
