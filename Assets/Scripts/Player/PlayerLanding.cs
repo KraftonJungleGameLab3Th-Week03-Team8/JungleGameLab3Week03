83861bb4b5824b6cd4db32d42d8151a863286a29
sayishuu <smile52673@gmail.com>
2025-03-23 18:49:56 +0900

    [FEAT] 랜딩 속도 지정 조건 : 에어스탑회전속도 + 높이
    충격 강도도 같은 원리로 구하면 됩니다.

--- Assets/Scripts/Player/PlayerLanding.cs
+++ Assets/Scripts/Player/PlayerLanding.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class PlayerLanding : MonoBehaviour
 {
-    private float _landForce = 10f;
+    private float _landForce = 15f;
     private float _chargedLandForce;
+    public float ChargedLandForce => _chargedLandForce;
 
     private void Start()
     {
@@ -13,7 +14,7 @@ public class PlayerLanding : MonoBehaviour
     private void Land(Rigidbody2D rb)
     {
         rb.angularVelocity = 0;
-        _chargedLandForce = Manager.Game.PlayerController.PlayerAirStop.RotateSpeed * 2;
+        _chargedLandForce = Manager.Game.PlayerController.PlayerAirStop.CurrentRotation / 200;
         rb.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         rb.constraints = RigidbodyConstraints2D.FreezePositionX;
         rb.AddForce(Vector2.down * (_landForce + _chargedLandForce), ForceMode2D.Impulse);
