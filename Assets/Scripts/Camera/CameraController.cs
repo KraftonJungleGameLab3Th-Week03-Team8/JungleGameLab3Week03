d7b8c1630d73f6d68816e2469e39f54b02bf3f91
bokob <bmc0520@naver.com>
2025-03-24 02:17:07 +0900

    [FEAT] Camera Shake

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,6 +5,9 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform _target;
     [SerializeField] CinemachineCamera _cinemachineCamera;
+    private float _shakeTimer;
+    private float _shakeTimerTotal;
+    private float _startingIntensity;
 
     public void Init(Transform target)
     {
@@ -15,5 +18,33 @@ public class CameraController : MonoBehaviour
         CameraTarget cameraTarget = new CameraTarget();
         cameraTarget.TrackingTarget = _target;
         _cinemachineCamera.Follow = _target;
+    }
+
+    public void ShakeCamera(float intensity = 1.5f, float time = 0.5f)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = GetComponent<CinemachineBasicMultiChannelPerlin>();
+        cinemachineBasicMultiChannelPerlin.AmplitudeGain = intensity;
+        _startingIntensity = intensity;
+        _shakeTimerTotal = time;
+        _shakeTimer = time;
+    }
+
+    public void ShakeTimer()
+    {
+        _shakeTimer -= Time.deltaTime;
+        if (_shakeTimer <= 0)
+        {
+            CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = GetComponent<CinemachineBasicMultiChannelPerlin>();
+
+            cinemachineBasicMultiChannelPerlin.AmplitudeGain = Mathf.Lerp(_startingIntensity, 0f, 1 - (_shakeTimer / _shakeTimerTotal));
+        }
+    }
+
+    private void Update()
+    {
+        if (_shakeTimer > 0)
+        {
+            ShakeTimer();
+        }
     }
 }
